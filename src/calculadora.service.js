800021eb4050db5a9b589fa86f9450f9eeb630e5
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-19 11:13:49 -0300

    Implementando operações

--- src/calculadora.service.js
+++ src/calculadora.service.js
@@ -1,12 +1,36 @@
 function CalculadoraService() {
 
+    const SOMA = '+'
+    const SUBTRACAO = '-'
+    const DIVISAO = '/'
+
     function calcular(numero1, numero2, operacao) {
-        return 0;
+        
+        let resultado;
+
+        switch(operacao) {
+            case  SOMA:
+                resultado = numero1 + numero2
+                break;
+            case SUBTRACAO:
+                resultado = numero1 - numero2
+                break
+            case DIVISAO:
+                resultado = numero1 / numero2
+                break
+            // case '*':
+            //     resultado = numero1 * numero2
+        }
+
+        return resultado
     }
 
     return [
-        calcular
-    ]
+        calcular,
+        SOMA,
+        SUBTRACAO,
+        DIVISAO
+    ];
 
 }
 
--- src/calculadora.service.test.js
+++ src/calculadora.service.test.js
@@ -4,11 +4,21 @@ import CalculadoraService from './calculadora.service'
 
 describe('Teste do CalculadoraService', () => {
     
-    const [calcular] = CalculadoraService()
+    const [calcular, SOMA, SUBTRACAO, DIVISAO] = CalculadoraService()
 
     it('deve garantir que 1 + 4 = 5.', () => {
-        let soma = calcular(1, 4, '+')
+        let soma = calcular(1, 4, SOMA)
         expect(soma).toEqual(5)
+    })
+
+    it('deve garantir que 1 - 4 = -3.', () => {
+        let subtracao = calcular(1, 4, SUBTRACAO)
+        expect(subtracao).toEqual(-3)
+    })
+
+    it('deve garantir que 1 / 4 = 0.25', () => {
+        let divisao = calcular(1, 4, DIVISAO)
+        expect(divisao).toEqual(0.25)
     })
 
 })
