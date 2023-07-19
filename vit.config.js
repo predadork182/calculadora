905ebaa2cab1c02307ce63d0c7839ee4775d9e7b
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-19 14:36:37 -0300

    teste

--- vit.config.js
+++ vit.config.js
@@ -1,5 +1,5 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from "vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
     plugins: [react()],
