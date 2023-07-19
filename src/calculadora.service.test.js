e1f522e765f1046a65f620ebaa4775ebd3710299
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-19 10:23:03 -0300

    Criação do arquivo caculadora.service com a função calcular

--- src/calculadora.service.test.js
+++ src/calculadora.service.test.js
@@ -2,4 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CalculadoraService from './calculadora.service'
 
-describe()
+describe('Teste do CalculadoraService', () => {
+    
+    const [calcular] = CalculadoraService()
+
+    it('deve garantir que 1 + 4 = 5.', () => {
+        let soma = calcular(1, 4, '+')
+        expect(soma).toEqual(5)
+    })
+
+})
---
+++ src/calculadora.servie.js
@@ -0,0 +1,13 @@
+function CalculadoraService() {
+
+    function calcular(numero1, numero2, operacao) {
+        return 0;
+    }
+
+    return [
+        calcular
+    ]
+
+}
+
+export default CalculadoraService
