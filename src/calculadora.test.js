8419954effddb292d94d724713d91132adad6fa4
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-19 14:44:02 -0300

    Updates

--- src/calculadora.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Calculadora from './calculadora';
-
-test('renders learn react link', () => {
-  render(<Calculadora />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
