2f90290606558e6dba85a66a578845bff0fc60ed
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-18 21:59:11 -0300

    Criando interface part 3/3

--- src/calculadora.js
+++ src/calculadora.js
@@ -21,6 +21,62 @@ function Calculadora() {
            readOnly='readonly' />
         </Col>
       </Row>
+      <Row>
+        <Col>
+          <Button variant='light'>7</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>8</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>9</Button> 
+        </Col>
+        <Col>
+          <Button variant='warning'>/</Button> 
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='light'>4</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>5</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>6</Button> 
+        </Col>
+        <Col>
+          <Button variant='warning'>*</Button> 
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='light'>1</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>2</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>3</Button> 
+        </Col>
+        <Col>
+          <Button variant='warning'>-</Button> 
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='light'>0</Button> 
+        </Col>
+        <Col>
+          <Button variant='light'>.</Button> 
+        </Col>
+        <Col>
+          <Button variant='success'>=</Button> 
+        </Col>
+        <Col>
+          <Button variant='warning'>+</Button> 
+        </Col>
+      </Row>
     </Container>
   );
 }
