36e8429e7f4b70a5d2f63ee17971622a8c9e5b4b
mikek182 <marcosvinicius@geekwork.com.br>
2023-07-20 16:37:26 -0300

    Criando ação de limpar

--- src/calculadora.js
+++ src/calculadora.js
@@ -6,15 +6,53 @@ import CalculadoraService from './calculadora.service'
 function Calculadora() {
 
   const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService()
-
   const [txtNumeros, setTxtNumeros] = useState('0');
+  const [numero1, setNumero1] = useState('0');
+  const [numero2, setNumero2] = useState(null);
+  const [operacao, setOperacao] = useState(null);
+  
 
   function adicionarNumero(numero){
-    setTxtNumeros(txtNumeros + numero)
+    let resultado; 
+    if (operacao === null) {
+      resultado = concatenarNumero(numero1, numero);
+      setNumero1(resultado)
+    } else {
+      resultado = concatenarNumero(numero2, numero);
+      setNumero2(resultado)
+    }
+    setTxtNumeros(resultado)
   }
 
   function definirOperacao(op){
-    setTxtNumeros(op)
+    // apenas define a operação caso ela não exista
+    if (operacao === null){
+      setOperacao(op)
+      return
+    }
+    // caso operação estiver definida e número 2 selecionado, realiza o calculo da operação
+    if (numero2 !== null){
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+      setOperacao(op)
+      setNumero1(resultado.toString())
+      setNumero2(null)
+      setTxtNumeros(resultado.toString())
+    }
+  }
+
+  function acaoCalcular(){
+    if (numero2 === null) {
+      return
+    }
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao)
+    setTxtNumeros(resultado);
+  }
+
+  function limpar(){
+    setTxtNumeros('0')
+    setNumero1('0')
+    setNumero2(null)
+    setOperacao(null)
   }
 
   return (
@@ -26,7 +64,7 @@ function Calculadora() {
       }}>
       <Row>
         <Col xs="3"> 
-          <Button variant='danger'>C</Button>
+          <Button variant='danger' onClick={() => limpar()}>C</Button>
         </Col>
         <Col xs="9"> 
            <Form.Control type='text'
@@ -83,13 +121,13 @@ function Calculadora() {
           <Button variant='light' onClick={() => adicionarNumero('0')}>0</Button> 
         </Col>
         <Col> 
-          <Button variant='light'>.</Button> 
+          <Button variant='light' onClick={() => adicionarNumero('.')}>.</Button> 
         </Col>
         <Col>
-          <Button variant='success'>=</Button> 
+          <Button variant='success' onClick={acaoCalcular}>=</Button> 
         </Col>
         <Col>
-          <Button variant='warning' onClick={() => definirOperacao('+')}>+</Button> 
+          <Button variant='warning' onClick={() => definirOperacao('+')}>+</Button>  
         </Col>
       </Row>
     </Container> 
